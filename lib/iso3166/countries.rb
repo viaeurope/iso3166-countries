452cd1b16e8c361ca14fe922eede8163558688d1
Ali Ismayilov <ali@ismailov.info>
2021-03-01 14:25:20 +0100

    Initial commit

---
+++ lib/iso3166/countries.rb
@@ -0,0 +1,10 @@
+# frozen_string_literal: true
+
+require_relative "countries/version"
+
+module ISO3166
+  module Countries
+    class Error < StandardError; end
+    # Your code goes here...
+  end
+end
---
+++ lib/iso3166/countries/version.rb
@@ -0,0 +1,7 @@
+# frozen_string_literal: true
+
+module ISO3166
+  module Countries
+    VERSION = "0.0.1"
+  end
+end
